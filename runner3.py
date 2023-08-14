d34b748c94fd7b3a4f832b3ea93e3bbf6e3debb0
Minh Tran <quangminhtran22@gmail.com>
2023-08-14 17:41:22 +0700

    Create runner3.py

---
+++ runner3.py
@@ -0,0 +1,226 @@
+    def validation(self, global_step):
+        self.model.eval()
+
+        dev_batches, num_steps, _, _ = self.iterator.get_batches(
+            "dev", self.cfg.batch_size, self.cfg.num_gpus)
+
+        dev_iterator = self.iterator.get_data_iterator(
+            dev_batches, self.cfg.task, self.cfg.ururu, self.cfg.add_auxiliary_task, self.cfg.context_size)
+
+        entities = self.iterator.get_entities(excluded_domains=self.cfg.excluded_domains)
+        entity_tensor = self.iterator.get_entity_tensor(entities)
+
+        reporter = Reporter(1000000, self.cfg.model_dir)
+
+        torch.set_grad_enabled(False)
+        for batch in tqdm(dev_iterator, total=num_steps, desc="Validation"):
+            start_time = time.time()
+
+            inputs, labels = batch
+
+            _, step_outputs = self.step_fn(inputs, entity_tensor, *labels)
+
+            reporter.step(start_time, lr=None, step_outputs=step_outputs, is_train=False)
+
+        do_span_stats = True if "span" in step_outputs else False
+        do_resp_stats = True if "resp" in step_outputs else False
+
+        reporter.info_stats("dev", global_step, do_span_stats, do_resp_stats)
+
+        torch.set_grad_enabled(True)
+
+    def finalize_bspn(self, belief_outputs, domain_history, constraint_history, span_outputs=None, input_ids=None):
+        eos_token_id = self.reader.get_token_id(definitions.EOS_BELIEF_TOKEN)
+
+        batch_decoded = []
+        for i, belief_output in enumerate(belief_outputs):
+            if belief_output[0] == self.reader.pad_token_id:
+                belief_output = belief_output[1:]
+
+            if eos_token_id not in belief_output:
+                eos_idx = len(belief_output) - 1
+            else:
+                eos_idx = belief_output.index(eos_token_id)
+
+            bspn = belief_output[:eos_idx + 1]
+
+            decoded = {}
+
+            decoded["bspn_gen"] = bspn
+
+            # update bspn using span output
+            if span_outputs is not None and input_ids is not None:
+                span_output = span_outputs[i]
+                input_id = input_ids[i]
+
+                #print(self.reader.tokenizer.decode(input_id))
+                #print(self.reader.tokenizer.decode(bspn))
+
+                eos_idx = input_id.index(self.reader.eos_token_id)
+                input_id = input_id[:eos_idx]
+
+                span_result = {}
+
+                bos_user_id = self.reader.get_token_id(definitions.BOS_USER_TOKEN)
+
+                span_output = span_output[:eos_idx]
+
+                b_slot = None
+                for t, span_token_idx in enumerate(span_output):
+                    turn_id = max(input_id[:t].count(bos_user_id) - 1, 0)
+                    turn_domain = domain_history[i][turn_id]
+
+                    if turn_domain not in definitions.INFORMABLE_SLOTS:
+                        continue
+
+                    span_token = self.reader.span_tokens[span_token_idx]
+
+                    if span_token not in definitions.INFORMABLE_SLOTS[turn_domain]:
+                        b_slot = span_token
+                        continue
+
+                    if turn_domain not in span_result:
+                        span_result[turn_domain] = defaultdict(list)
+
+                    if b_slot != span_token:
+                        span_result[turn_domain][span_token] = [input_id[t]]
+                    else:
+                        span_result[turn_domain][span_token].append(input_id[t])
+
+                    b_slot = span_token
+
+                for domain, sv_dict in span_result.items():
+                    for s, v_list in sv_dict.items():
+                        value = v_list[-1]
+                        span_result[domain][s] = self.reader.tokenizer.decode(
+                            value, clean_up_tokenization_spaces=False)
+
+                span_dict = copy.deepcopy(span_result)
+
+                ontology = self.reader.db.extractive_ontology
+
+                flatten_span = []
+                for domain, sv_dict in span_result.items():
+                    flatten_span.append("[" + domain + "]")
+
+                    for s, v in sv_dict.items():
+                        if domain in ontology and s in ontology[domain]:
+                            if v not in ontology[domain][s]:
+                                del span_dict[domain][s]
+                                continue
+
+                        if s == "destination" or s == "departure":
+                            _s = "destination" if s == "departure" else "departure"
+
+                            if _s in sv_dict and v == sv_dict[_s]:
+                                if s in span_dict[domain]:
+                                    del span_dict[domain][s]
+                                if _s in span_dict[domain]:
+                                    del span_dict[domain][_s]
+                                continue
+
+                        if s in ["time", "leave", "arrive"]:
+                            v = v.replace(".", ":")
+                            if re.match("[0-9]+:[0-9]+", v) is None:
+                                del span_dict[domain][s]
+                                continue
+                            else:
+                                span_dict[domain][s] = v
+
+                        flatten_span.append("[value_" + s + "]")
+                        flatten_span.append(v)
+
+                    if len(span_dict[domain]) == 0:
+                        del span_dict[domain]
+                        flatten_span.pop()
+
+                #print(flatten_span)
+
+                #input()
+
+                decoded["span"] = flatten_span
+
+                constraint_dict = self.reader.bspn_to_constraint_dict(
+                    self.reader.tokenizer.decode(bspn, clean_up_tokenization_spaces=False))
+
+                if self.cfg.overwrite_with_span:
+                    _constraint_dict = OrderedDict()
+
+                    for domain, slots in definitions.INFORMABLE_SLOTS.items():
+                        if domain in constraint_dict or domain in span_dict:
+                            _constraint_dict[domain] = OrderedDict()
+
+                        for slot in slots:
+                            if domain in constraint_dict:
+                                cons_value = constraint_dict[domain].get(slot, None)
+                            else:
+                                cons_value = None
+
+                            if domain in span_dict:
+                                span_value = span_dict[domain].get(slot, None)
+                            else:
+                                span_value = None
+
+                            if cons_value is None and span_value is None:
+                                continue
+
+                            # priority: span_value > cons_value
+                            slot_value = span_value or cons_value
+
+                            _constraint_dict[domain][slot] = slot_value
+                else:
+                    _constraint_dict = copy.deepcopy(constraint_dict)
+
+                bspn_gen_with_span = self.reader.constraint_dict_to_bspn(
+                    _constraint_dict)
+
+                bspn_gen_with_span = self.reader.encode_text(
+                    bspn_gen_with_span,
+                    bos_token=definitions.BOS_BELIEF_TOKEN,
+                    eos_token=definitions.EOS_BELIEF_TOKEN)
+
+                decoded["bspn_gen_with_span"] = bspn_gen_with_span
+
+            batch_decoded.append(decoded)
+
+        return batch_decoded
+
+    def finalize_resp(self, resp_outputs):
+        bos_action_token_id = self.reader.get_token_id(definitions.BOS_ACTION_TOKEN)
+        eos_action_token_id = self.reader.get_token_id(definitions.EOS_ACTION_TOKEN)
+
+        bos_resp_token_id = self.reader.get_token_id(definitions.BOS_RESP_TOKEN)
+        eos_resp_token_id = self.reader.get_token_id(definitions.EOS_RESP_TOKEN)
+
+        batch_decoded = []
+        for resp_output in resp_outputs:
+            resp_output = resp_output[1:]
+            if self.reader.eos_token_id in resp_output:
+                eos_idx = resp_output.index(self.reader.eos_token_id)
+                resp_output = resp_output[:eos_idx]
+
+            try:
+                bos_action_idx = resp_output.index(bos_action_token_id)
+                eos_action_idx = resp_output.index(eos_action_token_id)
+            except ValueError:
+                logger.warn("bos/eos action token not in : {}".format(
+                    self.reader.tokenizer.decode(resp_output)))
+                aspn = [bos_action_token_id, eos_action_token_id]
+            else:
+                aspn = resp_output[bos_action_idx:eos_action_idx + 1]
+
+            try:
+                bos_resp_idx = resp_output.index(bos_resp_token_id)
+                eos_resp_idx = resp_output.index(eos_resp_token_id)
+            except ValueError:
+                logger.warn("bos/eos resp token not in : {}".format(
+                    self.reader.tokenizer.decode(resp_output)))
+                resp = [bos_resp_token_id, eos_resp_token_id]
+            else:
+                resp = resp_output[bos_resp_idx:eos_resp_idx + 1]
+
+            decoded = {"aspn_gen": aspn, "resp_gen": resp}
+
+            batch_decoded.append(decoded)
+
+        return batch_decoded
